3fedd581b1896f20ecb302579bbb898924a28b36
Samuel Gomez <samuelgomez4010@gmail.com>
2022-12-10 21:18:04 -0500

    Movie creation

--- controller/movie.py
+++ controller/movie.py
@@ -1,18 +1,27 @@
 from application import app
 from model.repository import MovieRepository
 from model.entity import Movie
+from flask import request
 
 repository = MovieRepository()
+
 
 @app.route("/api/movies", methods=["GET"])
 def list():
     return repository.findAll()
 
+
 @app.route("/api/movies/<code>", methods=["GET"])
 def findByCode(code):
     return repository.findByCode(code)
 
+
 @app.route("/api/movies", methods=["POST"])
 def create():
-    movie = Movie()
+    code = request.json["code"]
+    name = request.json["name"]
+    image = request.json["image_url"]
+    year = request.json["year"]
+    movie = Movie(code, name, image_url=image, year=year)
     repository.insert(movie)
+    return "{ 'message': 'Success' }", 201
